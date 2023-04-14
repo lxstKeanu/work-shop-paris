fcfb5a690e6facbbcd35b6282b90870bf8d6f98e
lxstKeanu <dn1weplay98@gmail.com>
2023-04-14 05:20:43 +0300

    modal window update: added photos, styles. added opacity:hover to main photos

--- src/component/modal-window/modal-window.jsx
+++ src/component/modal-window/modal-window.jsx
@@ -3,6 +3,8 @@
 import { React } from "react";
 import { ReactComponent as Cancel } from "../../images/cancel.svg";
 import SocialMedia from "../social-media/social-media";
+import alesiaPhoto from "../../images/lesia.jpg";
+import leraPhoto from "../../images/lera.jpg";
 import "./modal-window.css";
 
 function ModalWindow({ showAddForm, handleAddFormHide }) {
@@ -23,12 +25,14 @@ function ModalWindow({ showAddForm, handleAddFormHide }) {
               <div className="modal-social-information">
                 <SocialMedia
                   name="Alesia"
+                  photo={alesiaPhoto}
                   ins="https://instagram.com/alesia__ag"
                   face="https://t.me/alesia_ag"
                 />
                 <div className="modal-line"></div>
                 <SocialMedia
                   name="Valeria"
+                  photo={leraPhoto}
                   ins="https://instagram.com/leka_fotografia"
                   face="https://t.me/ValeriiaChirun"
                 />
--- src/component/social-media/social-media.jsx
+++ src/component/social-media/social-media.jsx
@@ -4,10 +4,11 @@ import React from "react";
 import { ReactComponent as Instagram } from "../../images/instagram.svg";
 import { ReactComponent as Telegram } from "../../images/telegram.svg";
 
-function SocialMedia({ name, ins, face }) {
+function SocialMedia({ photo, name, ins, face }) {
   return (
     <div className="modal-social">
       <span className="modal-name">{name}</span>
+      <img className="modal-photo" src={photo} alt="photographer" />
       <div className="svg-icon">
         <a
           target="_blank"
